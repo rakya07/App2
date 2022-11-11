9bb2e55369bfa582eafdc6be92fe64d08351d3ba
Naveen Kumar <chinna.nk4@gmail.com>
2022-11-11 18:34:53 +0530

    #2 Added Inference api

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 #Importing required libraries
 import streamlit as st  # For web development
+import requests
 from sentence_transformers import SentenceTransformer   # For Sentence embeddings
 from sklearn.metrics.pairwise import cosine_similarity  # For Cosine similarity between Embeddings
 
 st.title("Sentence Textual Similarity") # Document title
+API_URL = "https://api-inference.huggingface.co/models/sentence-transformers/stsb-mpnet-base-v2"
+headers = {"Authorization": "Bearer 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"}
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
 
 @st.cache(allow_output_mutation=True) # Caching the model to avoid loading it again and again
 def question_answer(text1, text2): # Function to find similarity between two sentences
-    s = SentenceTransformer('stsb-mpnet-base-v2') # Loading the model
-    text1 = s.encode(text1) # Encoding the first sentence
-    text2 = s.encode(text2) # Encoding the second sentence
-    sim = cosine_similarity([text1],[text2]) # Finding the cosine similarity between the two sentences
-    norm_sim = (sim+1)/2 # Normalizing the similarity score
-    return norm_sim.item() # Returning the normalized similarity score
+    payload = {"inputs": {"source_sentence": text1, "sentences": [text2]}}
+    sim = query(payload)
+    norm_sim = (sim[0]+1)/2 # Normalizing the similarity score
+    return norm_sim # Returning the normalized similarity score
 
 st.subheader("Enter the two sentences to compare") # Subheader for the document
 with st.form('form'):
